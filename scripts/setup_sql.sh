d73039557c6144f758f035bc84f2d437cc7f4cb6
malagovind <mala.govindan@gmail.com>
2020-06-02 23:18:33 +0530

    Update setup_sql.sh

--- scripts/setup_sql.sh
+++ scripts/setup_sql.sh
@@ -12,3 +12,6 @@ gcloud sql users create ${SQL_USER} --password ${SQL_PASSWORD} -i  ${SQL_INSTANC
 
 SQL_HOST=$(gcloud sql instances describe ${SQL_INSTANCE_NAME} | grep "\- ipAddress" | cut -d: -f2 | cut -c2-)
 export SQL_HOST
+
+POSTGRES_HOST=$(gcloud sql instances describe ${SQL_INSTANCE_NAME} | grep "\- ipAddress" | cut -d: -f2 | cut -c2-)
+export POSTGRES_HOST
